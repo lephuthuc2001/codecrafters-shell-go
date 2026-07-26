4ced272159c1e718787c8e1ad96a3bc08ef4c64e
Le Phu Thuc <lephuthuc2001@gmail.com>
2026-07-26 21:13:58 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,20 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
-func splitCommandInput(input string) (command string, commandArguments string) {
+func splitCommandInput(input string) (command string, commandArguments []string) {
 	input = strings.Trim(input, "\n")
 	input = strings.TrimSpace(input)
 
 	inputFields := strings.Fields(input)
 
-	return inputFields[0], strings.Join(inputFields[1:], " ")
+	return inputFields[0], inputFields[1:]
+}
+
+func isBuiltInCommand(command string) bool {
+	builtInCommand := []string{
+		"type",
+		"echo",
+		"exit",
+	}
+
+	return slices.Contains(builtInCommand, command)
 }
 
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
@@ -36,10 +46,19 @@ func main() {
 		case "exit":
 			return
 		case "echo":
-			fmt.Println(commandArguments)
+			fmt.Println(strings.Join(commandArguments, " "))
+		case "type":
+			typeCheckCommand := commandArguments[0]
+
+			if isBuiltInCommand(typeCheckCommand) {
+				fmt.Printf("%v is a shell builtin", command)
+			} else {
+				fmt.Printf("%v: command not found", command)
+			}
+			fmt.Print("\n")
+
 		default:
 			fmt.Printf("%v: command not found", command)
-
 			fmt.Print("\n")
 		}
 	}
