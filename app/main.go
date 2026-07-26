9e7583ce25ba0c84b9880de0ebb2434d00f0e367
Le Phu Thuc <lephuthuc2001@gmail.com>
2026-07-26 20:48:32 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,6 +7,15 @@ import (
 	"strings"
 )
 
+func splitCommand(str string) (command string, commandArguments string) {
+	str = strings.Trim(str, "\n")
+	str = strings.TrimSpace(str)
+
+	strFields := strings.Fields(str)
+
+	return strFields[0], strings.Join(strFields[1:]," ")
+}
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
@@ -14,18 +23,24 @@ func main() {
 	for {
 		fmt.Print("$ ")
 
-		command, error := reader.ReadString('\n')
+		str, error := reader.ReadString('\n')
 
 		if error != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", error)
 			os.Exit(1)
 		}
 
-		command = strings.TrimSpace(strings.Trim(command, "\n"))
+		command, commandArguments  := splitCommand(str)
 
 		if command == "exit"{
 			break
 		}
+
+		if command == "echo" {
+			fmt.Println(commandArguments)
+			continue
+		}
+
 
 		fmt.Printf("%v: command not found", command)
 
