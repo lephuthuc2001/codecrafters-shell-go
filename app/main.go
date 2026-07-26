48785c4b0e41eaa14d7eed17b5b7a0ab39ba9409
Le Phu Thuc <lephuthuc2001@gmail.com>
2026-07-26 18:17:54 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Print("$ ") 
+
+	var count int;
+	for {
+		if count == 0 {
+			fmt.Print("$ ") 
+		} else{
+			fmt.Print("\n$ ") 
+		}
 
 	reader :=  bufio.NewReader(os.Stdin)
 	command, error := reader.ReadString('\n');
 
 	if error != nil {
-		log.Fatal("xx")
+		fmt.Fprintln(os.Stderr, "Error reading input:", error)
+		os.Exit(1)
 	}
 
 	fmt.Printf("%v: command not found",strings.Trim(command,"\n"))
+
+	count ++
+	}
 }
